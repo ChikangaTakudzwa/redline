c8a9988a5427a50615873e5ba660c7d861a590e5
ChikangaTakudzwa <chikangatakudzwa@outlook.com>
2023-04-09 13:10:24 +0000

    Added category section

--- ui/views.py
+++ ui/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(requests):
     return render (requests, "home/index.html")
+
+
+def shop(requests):
+    return render (requests, "shop/index.html")
